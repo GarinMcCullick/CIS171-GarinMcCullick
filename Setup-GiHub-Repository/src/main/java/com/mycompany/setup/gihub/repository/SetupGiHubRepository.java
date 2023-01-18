e169a4726944c5b364a57ed5770a7406a0742bd5
Garin Mccullick <glmccullick@gmail.com>
2023-01-18 17:48:48 -0600

    trying to commit

---
+++ Setup-GiHub-Repository/src/main/java/com/mycompany/setup/gihub/repository/SetupGiHubRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.setup.gihub.repository;
+
+/**
+ *
+ * @author AceFly'N
+ */
+public class SetupGiHubRepository {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

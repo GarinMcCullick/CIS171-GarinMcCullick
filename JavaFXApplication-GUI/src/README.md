bf205d24751638860cc296b044177e95d49b8466
Garin Mccullick <glmccullick@gmail.com>
2023-02-07 22:16:29 -0600

    committing....

---
+++ JavaFXApplication-GUI/src/README.md
@@ -0,0 +1,52 @@
+# SETUP EXAMPLE
+
+This Project is an app that uses the javafx library to create gui's and displays.
+
+## Example Output
+
+
+![Sample Output](README.jpg)
+
+## Analysis Steps
+
+When making this display, I thought about the setup and how it should be organized. I used several imports from the javafx library as well.
+
+### Design
+
+How did you approach your program design? Did you use multiple classes to define various objects?
+I desigined this app with a header at the top and a button on the bottom of the display.
+
+### Testing / Notes
+
+To test this app you have to run the program from the file to see the display.
+
+## Do not change content below this line
+## Adapted from a README Built With
+
+* [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - The web framework used
+* [Maven](https://maven.apache.org/) - Dependency Management
+* [ROME](https://rometools.github.io/rome/) - Used to generate RSS Feeds
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+* **Billie Thompson** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone who's code was used
+* Inspiration
+* etc
---
+++ JavaFXApplication-GUI/src/javafxapplication/gui/JavaFXApplicationGUI.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package javafxapplication.gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author AceFly'N
+ */
+public class JavaFXApplicationGUI extends Application {
+    
+    @Override
+  public void start(Stage Stage) {
+    
+    Label heading = new Label("Selling My Sailboat!");//heres the heading
+    heading.setFont(Font.font("Arial", FontWeight.BOLD, 16));//setting font styles
+    
+    Image image = new Image("https://images.unsplash.com/photo-1611195715369-f51eb50b6722?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2FpbGJvYXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60");//heres the image
+    ImageView imageView = new ImageView(image);//ImageView is like the container for the image 
+    
+    
+    
+    
+    
+    TextArea textArea = new TextArea("this is my 1998 monohaul. \n I'm hoping to sell to an adventurous person who can get more use out of it!");// this is a text area
+    
+    Button button = new Button("More Info");// heres a button
+    //////////////////////////////////////////tieing together in vbox
+    VBox root = new VBox(heading, imageView, textArea,button);// this is the root node and add all the elements are tied into it; Vbox is like a parent container that arranges the children into a column
+    
+    root.setPrefWidth(500);
+    root.setPrefHeight(500);
+    /////////////////////////////////////////////////////////////////////// creating the scene and stage
+    Scene scene = new Scene(root);//scene is the top level container basically or like the body of html almost and stage is like the <html> tag or container for comparison
+    Stage.setScene(scene);
+    Stage.show();
+  }
+  
+  public static void main(String[] args) {
+    launch(args);
+    
+}}
